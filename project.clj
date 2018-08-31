c14f1eda3655f9f1cf8d1803c9b43e13e13054d1
FerdiKuehne <ferdi.kuehne@gmx.net>
2018-08-31 13:32:55 +0200

    Add fressian for clojurescript lib.

--- project.clj
+++ project.clj
@@ -1,12 +1,22 @@
-(defproject io.replikativ/incognito "0.2.2"
+(defproject io.replikativ/incognito "0.2.3 - Snapshot"
   :description "Safe transport of unknown record types in distributed systems."
   :url "https://github.com/replikativ/incognito"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha14" :scope "provided"]
+                 [org.clojure/clojurescript "1.9.946" :scope "provided"]
                  [org.clojure/data.fressian "0.2.1" :scope "provided"]
                  [com.cognitect/transit-clj "0.8.285" :scope "provided"]
+                 [com.cognitect/transit-cljs "0.8.239" :scope "provided"]
+                 [fress "0.2.0-SNAPSHOT"]]
 
-                 [com.cognitect/transit-cljs "0.8.239" :scope "provided"]]
+  :profiles {:dev {:dependencies [[com.cemerick/piggieback "0.2.1"]]
+                   :figwheel {:nrepl-port 7888
+                              :nrepl-middleware ["cider.nrepl/cider-middleware"
+                                                 "cemerick.piggieback/wrap-cljs-repl"]}
+                   :plugins [[lein-figwheel "0.5.8"]]}
+             :test {:dependencies [[clj-time "0.13.0"]]}}
 
-  :profiles {:test {:dependencies [[clj-time "0.13.0"]]}})
+
+
+  )
