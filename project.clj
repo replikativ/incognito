c321a64077a28b7bec82c510b3f6d5811f9d16d6
FerdiKuehne <ferdi.kuehne@gmx.net>
2018-09-06 10:38:07 +0200

    Update fress version for clojurescript.

--- project.clj
+++ project.clj
@@ -8,7 +8,7 @@
                  [org.clojure/data.fressian "0.2.1" :scope "provided"]
                  [com.cognitect/transit-clj "0.8.285" :scope "provided"]
                  [com.cognitect/transit-cljs "0.8.239" :scope "provided"]
-                 [fress "0.2.0-SNAPSHOT"]]
+                 [fress "0.2.0"]]
 
   :plugins [[lein-cljsbuild "1.1.7"]]
 
