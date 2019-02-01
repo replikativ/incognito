129e59c6b50e3b57429d0741ebb5ed1b2afb6091
FerdiKuehne <ferdi.kuehne@gmx.net>
2019-02-01 19:40:05 +0100

    Update clj, cljs, transit-clj and transit-cljs deps.

--- project.clj
+++ project.clj
@@ -3,11 +3,11 @@
   :url "https://github.com/replikativ/incognito"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha14" :scope "provided"]
-                 [org.clojure/clojurescript "1.9.946" :scope "provided"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [org.clojure/clojurescript "1.10.439" :scope "provided"]
                  [org.clojure/data.fressian "0.2.1" :scope "provided"]
-                 [com.cognitect/transit-clj "0.8.285" :scope "provided"]
-                 [com.cognitect/transit-cljs "0.8.239" :scope "provided"]
+                 [com.cognitect/transit-clj "0.8.313"]
+                 [com.cognitect/transit-cljs "0.8.256"]
                  [fress "0.3.1"]]
 
   :plugins [[lein-cljsbuild "1.1.7"]]
